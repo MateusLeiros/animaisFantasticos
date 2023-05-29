2f9945960d8df311747987c68a8db3e58c3104f1
Mateus Leiros <mateusleiros@gmail.com>
2023-05-29 16:30:35 -0300

    Alterados arquivos js para separação em módulos. Adicionado uso de dataset.

---
+++ js/modules/scroll-animacao.js
@@ -0,0 +1,21 @@
+export default function initAnimacaoScroll() {
+  const sections = document.querySelectorAll('[data-anime="scroll"]');
+  if (sections.length) {
+    const windowParte = window.innerHeight * 0.7;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowParte < 0;
+        if (isSectionVisible) {
+          section.classList.add("ativo");
+        } else {
+          section.classList.remove("ativo");
+        }
+      });
+    }
+    animaScroll();
+
+    window.addEventListener("scroll", animaScroll);
+  }
+}

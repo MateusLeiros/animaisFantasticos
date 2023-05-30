b21240229e5257886bd907941cba742f7292566d
Mateus Leiros <mateusleiros@gmail.com>
2023-05-30 11:17:01 -0300

    Adicionada função de tooltip sobre o mapa

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
+import initToolTip from "./modules/tooltip.js";
 
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 initTabNav();
 initModal();
+initToolTip();

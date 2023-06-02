723fb1614549e935729fa2ad03f267a6bad3f998
Mateus Leiros <mateusleiros@gmail.com>
2023-06-02 16:33:04 -0300

    adicionado versão mobile do menu

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ import initTabNav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
 import initToolTip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
+import initMenuMobile from "./modules/menu-mobile.js";
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -13,3 +14,4 @@ initTabNav();
 initModal();
 initToolTip();
 initDropdownMenu();
+initMenuMobile();
